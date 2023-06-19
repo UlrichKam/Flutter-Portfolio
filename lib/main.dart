f7d8e109156263ec571447f43625ef5704c6a72e
UlrichKam <kam.Simon.ulrich@gmail.com>
2023-06-19 09:54:39 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:portfolio/pages/home/home.dart';
+import 'package:portfolio/utils/constants.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Portfolio",
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.dark,
+      darkTheme: Theme.of(context).copyWith(
+        platform: TargetPlatform.android,
+        scaffoldBackgroundColor: kBackgroundColor,
+        primaryColor: kPrimaryColor,
+        canvasColor: kBackgroundColor,
+        textTheme: GoogleFonts.latoTextTheme(),
+      ),
+      builder: (context, widget) => ResponsiveBreakpoints.builder(
+        breakpoints: [
+          const Breakpoint(start: 0, end: 450, name: MOBILE),
+          const Breakpoint(start: 451, end: 800, name: TABLET),
+          const Breakpoint(start: 801, end: 1920, name: DESKTOP),
+          const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
+        ], child: widget!,
+      ),
+      home: const Home(),
+    );
+  }
+}
